009c3cfbd11b903e8d59a96aa6bee654aefead75
sujin <whsrlgks@gmail.com>
2024-09-10 12:55:59 +0900

    충돌 문제로 수정했습니다

--- LG_CX_Project/src/main/java/com/LgCxProject/domain/medical_info/Mydate.java
+++ LG_CX_Project/src/main/java/com/LgCxProject/domain/medical_info/Mydate.java
@@ -1,4 +1,5 @@
 package com.LgCxProject.domain.medical_info;
+import com.LgCxProject.domain.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
